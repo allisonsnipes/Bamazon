20f3f904dcd69621e47ef5a751037aec984c2261
Allison Snipes <ajsnipes@usfca.edu>
2019-06-02 09:47:15 -0700

    js

--- assets/js/bamazonCustomer.js
+++ assets/js/bamazonCustomer.js
@@ -9,6 +9,37 @@ const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "nodeUser", //username
-  password: '',
-  database: "bamazon"
-});
+  password: '', //password
+  database: "bamazon" //name of db
+})
+
+//being able to throw errors
+connection.connect(function(err) {
+  if (err) throw err
+  console.log("connection working: " + connection.threadId);
+})
+
+//set up the function to display items for sale
+//use the select * (all) from name of the table
+function show() {
+  //start first with the connection.query
+  connection.query("SELECT * FROM bamazon", function (err, data){
+    if (err) throw err
+
+    var table = new Table({
+      //set up table for data by placing its headers
+      head: ["sku", "item", "department", "price", "stock"],
+      //determine column widths
+      colWidths: [10, 20, 15, 10, 10]
+    })
+
+    //table is an array so we can make a for loop for each new item we add to the table
+    for (var i = 0; i < res.length; i++) {
+      table.push(
+        [res[i].sku, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity])
+    }
+    console.log(table.toString())
+    restar9 
+    
+  })
+}
